5d2c915e33bea5b9974f9be26273be73022ccf54
DaniellaDeLauri <64893185+DaniellaDeLauri@users.noreply.github.com>
2024-05-18 12:08:06 +0300

    Add files via upload

---
+++ globall.py
@@ -0,0 +1,94 @@
+from mafia.seven_to_fourteen import what_role
+
+class blya():
+    def method(self):
+        self.nn = get_number_players()
+        self.aa = get_roles_players()
+        self.all_vars = get_all_vars()
+        self.name_clicked = None
+        self.max_alive = None
+
+
+def get_number_players():
+    global number_players
+    return number_players
+
+
+def get_names_players():
+    global names_players
+    return names_players
+
+
+def get_roles_players():
+    global roles_players
+    return roles_players
+
+
+def append_names_players(m):
+    global names_players
+    names_players.append(m)
+
+
+def set_roles_players(m):
+    global roles_players
+    roles_players = m
+
+
+def get_all_vars():
+    global all_vars
+    n = get_number_players()
+    a = get_roles_players()
+    c = create_dict()
+    if n >= 7:
+        player1 = None
+        player1 = what_role(a, 0, player1, c)
+        all_vars.append(player1)
+        player2 = None
+        player2 = what_role(a, 1, player2, c)
+        all_vars.append(player2)
+        player3 = None
+        player3 = what_role(a, 2, player3, c)
+        all_vars.append(player3)
+        player4 = None
+        all_vars.append(player4)
+        player5 = None
+        player5 = what_role(a, 4, player5, c)
+        all_vars.append(player5)
+        player6 = None
+        player6 = what_role(a, 5, player6, c)
+        all_vars.append(player6)
+        player7 = None
+        player7 = what_role(a, 6, player7, c)
+        all_vars.append(player7)
+        if n >= 8:
+            player8 = None
+            player8 = what_role(a, 7, player8, c)
+            all_vars.append(player8)
+            if n >= 9:
+                player9 = None
+                player9 = what_role(a, 8, player9, c)
+                all_vars.append(player9)
+                if n >= 10:
+                    player10 = None
+                    player10 = what_role(a, 9, player10, c)
+                    all_vars.append(player10)
+                    if n >= 11:
+                        player11 = None
+                        player11 = what_role(a, 10, player11, c)
+                        all_vars.append(player11)
+                        if n >= 12:
+                            player12 = None
+                            player12 = what_role(a, 11, player12, c)
+                            all_vars.append(player12)
+
+def create_dict():
+    c = {}
+    for i in get_names_players():
+        for j in get_roles_players():
+            c[str(i)] = str(j)
+    return c
+
+roles_players = []
+names_players = []
+all_vars = []
+number_players = 0
